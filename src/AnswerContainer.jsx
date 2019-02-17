b205e54993f8502e34a37b3b59aab0f6227195d6
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-02-17 12:50:26 -0800

    hooked up get request to api in parent component

--- src/AnswerContainer.jsx
+++ src/AnswerContainer.jsx
@@ -2,10 +2,34 @@ import React, { Component } from 'react';
 import Answer from './Answer';
 import Category from './Category';
 import Button from './Button';
+import axios from 'axios'; 
 // import './AnswerContainer.css';
 
 class AnswerContainer extends Component {
+  constructor(props){
+    super(props); 
+    this.state = {
+      categories : [], 
+      error: false
+    }
+    this.getClues = this.getClues.bind(this)
+  }
+
+ async getClues(){
+    try {
+      const data = await axios.get('http://jservice.io/api/random'); 
+      console.log('DATA', data); 
+    } catch (error){
+      this.setState({
+        error: true
+      })
+    }
+  }
   static defaultProps = {};
+
+  async componentDidMount(){
+    this.getClues(); 
+  }
   render() {
     return (
       <div className="AnswerContainer">
